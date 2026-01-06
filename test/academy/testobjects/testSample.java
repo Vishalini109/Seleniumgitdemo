5c98242508a4ab20f0f3838b6f35d589f1c1eeb2
vishalini <vishalinisingaravelu@gmail.com>
2026-01-06 14:45:37 +0530

    This is commit from gitdemo

--- test/academy/testobjects/testSample.java
+++ test/academy/testobjects/testSample.java
@@ -3,8 +3,11 @@ package academy.testobjects;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.testng.annotations.Test;
+
 public class testSample {
 
+	@Test(dataProvider="dataDrivenTestNG")
 	public static void main(String[] args) throws IOException {
 			dataDrivenTestNG d = new dataDrivenTestNG();
 			Object[][] data	= d.getData();
